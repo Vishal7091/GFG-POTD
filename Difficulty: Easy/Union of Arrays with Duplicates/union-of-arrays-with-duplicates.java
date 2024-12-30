8d318147feef5b6d92f6dcff18bdda89dc5f23c7
Vishal7091 <133277544+Vishal7091@users.noreply.github.com>
2024-12-30 23:56:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            // Reading the first array
+            String line1 = read.readLine().trim();
+            String[] numsStr1 = line1.split(" ");
+            int[] a = new int[numsStr1.length];
+            for (int i = 0; i < numsStr1.length; i++) {
+                a[i] = Integer.parseInt(numsStr1[i]);
+            }
+
+            // Reading the second array
+            String line2 = read.readLine().trim();
+            String[] numsStr2 = line2.split(" ");
+            int[] b = new int[numsStr2.length];
+            for (int i = 0; i < numsStr2.length; i++) {
+                b[i] = Integer.parseInt(numsStr2[i]);
+            }
+
+            // Creating an instance of the Solution class
+            Solution ob = new Solution();
+
+            // Calling doUnion method and printing the result
+            System.out.println(ob.findUnion(a, b));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int findUnion(int a[], int b[]) {
+        // code here
+          int ans=0;
+        // Create a Hashmap
+        HashMap<Integer,Integer>nm = new HashMap<>();
+        
+        // Inserting the value of array1 in the hashMap and increase the count
+        for(int x :a){
+            if(nm.get(x)==null) 
+            ans++;
+            nm.put(x,1);
+        }
+         for( int x:b){
+            if(nm.get(x)==null)
+            ans++;
+            nm.put(x,1);
+        }
+        return ans;
+    }
+}
